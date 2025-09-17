e13fcdafc32d7c89e1ce720890385974f3488e25
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-09-17 10:49:31 +0200

    feat: creat my first App

---
+++ my-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello this is my first page with React</h1>
+      <p>I am curentlly learning React.js</p>
+    </div>
+  )
+}
+
+export default App
