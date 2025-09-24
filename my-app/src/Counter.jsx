74f66c5038ecb75aa85e36ae45ed311dd9d0c30a
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-09-24 13:56:09 +0200

    feat: add button setCount to 0

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -12,6 +12,7 @@ function Counter() {
         <p>Count: {count}</p>
         <button onClick={() => setCount(count + 1)}> Increase</button>
         <button onClick={() => setCount(count - 1)}> Decrease</button>
+        <button onClick={() => setCount(0)}>Set count from 0</button>
     </div>
   )
 }
