0da2ea040273fa24b287e1fe416e7e58c45173dd
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-09-27 16:37:58 +0200

    feat: create Card and use props.children

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import TextInput from "./TextInput";
 import LoginForm from "./LoginForm";
 import UncontrolledInput from "./UncontrolledInput";
 import ItemList from "./ItemList";
+import Card from "./Card";
 
 function App() {
 
@@ -21,6 +22,20 @@ function App() {
       <UncontrolledInput />
       <h5 style={{ fontSize: "20px" }}>Render List of elements</h5>
       <ItemList items={myItems} />
+      <h5 style={{ fontSize: "20px" }}>Use props.children </h5>
+      <Card>
+        <h3>Prima Card</h3>
+        <p>Questo contenuto è passato come children al componente Card.</p>
+      </Card>
+
+      <Card>
+        <h3>Seconda Card</h3>
+        <ul>
+          <li>Elemento A</li>
+          <li>Elemento B</li>
+        </ul>
+      </Card>
+
     </div>
   );
 }
