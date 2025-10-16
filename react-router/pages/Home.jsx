9373fc7b309473f2394facac54f7693d022ff4e9
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-10-16 22:51:16 +0200

    feat: Add About and Home pages with routing
    - Implemented About page with a link to Home
    - Created Home page that fetches and displays todos from an API
    - Added loading and error handling states in Home component
    - Updated package dependencies for react-router and set-cookie-parser
    - Introduced scheduler module for improved task scheduling

---
+++ react-router/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
+
+function Home() {
+
+    // Stato locale per salvare i dati ricevuti dall'API
+    const [todos, setTodos] = useState([]);
+    const [laoding, setLoading] = useState(true);// per gestire il caricamento
+    const [error, setError] = useState(null);// per gestire eventuali errori
+
+
+    // useEffect serve per eseguire il fetch quando il componente viene caricato
+    useEffect(() => {
+        fetch("https://dummyjson.com/todos")
+        .then((res) => res.json()) // res.json() serve per convertire la risposta HTTP (in formato testo JSON) in un oggetto JavaScript per usare nel codice
+        .then((data) => {
+            setTodos(data.todos); // salviamo solo l’array "todos" dentro lo stato
+            setLoading(false);  // fine caricament
+        });
+    }, []); // [] significa che esegue solo una volta al montaggio
+
+
+    //Gestione stati diversi
+    if(laoding) return <p>Loading..</p>;
+    if(error) return <p>Error: {error}</p>;
+
+
+    return (
+        <div style={{textAlign: "center", marginTop: "20px"}}>
+        <h1>Home Page</h1>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}> {todo.todo} </li>
+            ))}
+        </ul>
+        <Link to="/about">About Page</Link>
+        </div>
+    );
+}
+
+export default Home;
