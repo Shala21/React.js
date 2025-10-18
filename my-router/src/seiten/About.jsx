2c75a4b3dc602f57d62be9cf032fc19fb4073f59
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-10-18 04:14:24 +0200

    feat: enhance About page with navigation functionality

--- my-router/src/seiten/About.jsx
+++ my-router/src/seiten/About.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 //poiche aggiungo layout comune non mi serve piu Link
-import { Link } from "react-router-dom"
+//import { Link } from "react-router-dom"
 
 function About() {
+    const navigate = useNavigate(); // l’hook va qui, nel componente principale
+
+    const goBack = () => {
+        navigate("/home"); // (attenzione: meglio /home tutto minuscolo se la route è così definita)
+    };
+
     return (
         <div style={{ textAlign: "center", marginTop: "20px" }}>
             <h1>About Page</h1>
             <p>This page contains important information about this project!</p>
+
+            <button onClick={goBack} >Go Back To Home Page</button>
+
+            {/* resta come commento perche uso useNavigate
             <Link to="/">Go to Home</Link>
+            */}
         </div>
     );
 }
